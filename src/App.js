791dcbae9b6b744cdfa52df553e6e26accdb9d5e
Stefan Sundin <git@stefansundin.com>
2021-11-27 21:21:17 -0800

    Clean out the cruft.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <h1>
+        React Demo for{' '}
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://www.npmjs.com/package/qr-detector"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          QrDetector
         </a>
-      </header>
+      </h1>
     </div>
   );
 }
