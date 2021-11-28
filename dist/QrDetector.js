82ddf89e2c7d9c3e4f9da6124493e9501a82d25a
Stefan Sundin <git@stefansundin.com>
2021-11-27 17:54:45 -0800

    Build v0.0.2.

--- dist/QrDetector.js
+++ dist/QrDetector.js
@@ -39,21 +39,21 @@ class QrDetector {
         if (!result) {
             return [];
         }
-        const { x, y } = result.location.topLeftCorner;
-        const bottomRight = result.location.bottomRightCorner;
-        const boundingBox = new DOMRectReadOnly(x, y, bottomRight.x - x, bottomRight.y - y);
-        const cornerPoints = [
-            result.location.topLeftCorner,
-            result.location.topRightCorner,
-            result.location.bottomRightCorner,
-            result.location.bottomLeftCorner,
-        ];
+        const minX = Math.min(result.location.topLeftCorner.x, result.location.topRightCorner.x, result.location.bottomRightCorner.x, result.location.bottomLeftCorner.x);
+        const minY = Math.min(result.location.topLeftCorner.y, result.location.topRightCorner.y, result.location.bottomRightCorner.y, result.location.bottomLeftCorner.y);
+        const maxX = Math.max(result.location.topLeftCorner.x, result.location.topRightCorner.x, result.location.bottomRightCorner.x, result.location.bottomLeftCorner.x);
+        const maxY = Math.max(result.location.topLeftCorner.y, result.location.topRightCorner.y, result.location.bottomRightCorner.y, result.location.bottomLeftCorner.y);
         return [
             {
                 format: 'qr_code',
                 rawValue: result.data,
-                boundingBox,
-                cornerPoints,
+                boundingBox: new DOMRectReadOnly(minX, minY, maxX - minX, maxY - minY),
+                cornerPoints: [
+                    result.location.topLeftCorner,
+                    result.location.topRightCorner,
+                    result.location.bottomRightCorner,
+                    result.location.bottomLeftCorner,
+                ],
             },
         ];
     }
